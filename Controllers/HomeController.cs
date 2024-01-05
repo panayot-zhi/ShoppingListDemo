c55e75ffd74d67b359607e5d8fa97bf2a03680c1
Panayot Ivanov <panayot_zhi@abv.bg>
2024-01-05 13:27:25 +0200

    Very basic error handling.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using ShoppingListDemo.Models;
 using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace ShoppingListDemo.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment _environment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
 
         public IActionResult Index()
@@ -26,7 +30,30 @@ namespace ShoppingListDemo.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = HttpContext.TraceIdentifier
+            };
+
+            if (Activity.Current is not null)
+            {
+                errorViewModel.RequestId = Activity.Current.Id;
+            }
+
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionHandlerPathFeature != null)
+            {
+                var error = exceptionHandlerPathFeature.Error;
+                errorViewModel.Path = exceptionHandlerPathFeature.Path;
+                errorViewModel.ErrorMessage = error.Message;
+                errorViewModel.StackTrace = error.StackTrace;
+            }
+
+            _logger.LogError("{RequestId} ({Path}): {ErrorMessage}",
+                errorViewModel.RequestId, errorViewModel.Path, errorViewModel.ErrorMessage);
+
+            return View(errorViewModel);
         }
     }
 }
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,8 +2,12 @@ namespace ShoppingListDemo.Models
 {
     public class ErrorViewModel
     {
+        public string? Path { get; set; }
+
         public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string? ErrorMessage { get; set; }
+
+        public string? StackTrace { get; set; }
     }
 }
