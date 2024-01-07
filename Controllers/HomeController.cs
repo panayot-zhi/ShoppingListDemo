82c1dec0e1560f42ca2668b77a9103b951bdfb3c
Panayot Ivanov <panayot_zhi@abv.bg>
2024-01-07 11:35:28 +0200

    Implement ajax methods.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using ShoppingListDemo.Data;
 using ShoppingListDemo.Models;
 using System.Diagnostics;
-using System.Security.Cryptography;
 
 namespace ShoppingListDemo.Controllers
 {
@@ -19,31 +18,95 @@ namespace ShoppingListDemo.Controllers
             _context = context;
         }
 
-        // GET: ScheduledShoppingItems
         public async Task<IActionResult> Index([FromQuery] DateTime? at)
         {
-            IQueryable<ScheduledShoppingItem> scheduledShoppingItemsQuery = _context.ScheduledShoppingItems;
-            IQueryable<ShoppingItem> shoppingItemsQuery = _context.ShoppingItems
-                .Include(s => s.ShoppingCategory);
+            at ??= DateTime.Now;
 
-            if (at.HasValue)
-            {
-                scheduledShoppingItemsQuery = scheduledShoppingItemsQuery
-                    .Include(x => x.ShoppingItem)
-                        .ThenInclude(x => x.ShoppingCategory)
-                    .Where(x => x.Day == at.Value.Date);
-            }
+            // NOTE: If we are retrieving the whole table at least limit the query!
+            var allScheduledShoppingItems = await _context.ScheduledShoppingItems
+                .Where(x => at.Value.AddMonths(-2) <= x.Day && x.Day <= at.Value.AddMonths(2))
+                .OrderBy(x => x.Day)
+                .ToListAsync();
 
-            var scheduledShoppingItems = await scheduledShoppingItemsQuery.ToListAsync();
-            var shoppingItems = await shoppingItemsQuery.ToListAsync();
+            var scheduledShoppingItemsCountPerDay = allScheduledShoppingItems
+                .GroupBy(x => x.Day)
+                .ToDictionary(x => x.Key.ToString("yyyy-MM-dd"), x => x.Count());
+
+            var scheduledShoppingItems = await _context.ScheduledShoppingItems
+                .Include(x => x.ShoppingItem)
+                    .ThenInclude(x => x!.ShoppingCategory)
+                .Where(x => x.Day == at.Value.Date)
+                .ToListAsync();
+
+            var shoppingItems = await _context.ShoppingItems
+                .Include(s => s.ShoppingCategory)
+                .ToListAsync();
+
             var viewModel = new ShoppingListViewModel()
             {
-                CurrentDate = at ?? DateTime.Now,
+                CurrentDate = at.Value,
                 CurrentShoppingItems = scheduledShoppingItems,
+                ScheduledShoppingItemsCountPerDay = scheduledShoppingItemsCountPerDay,
                 AllShoppingItems = shoppingItems
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost("addItem")]
+        public async Task<IActionResult> AddItem([FromBody] AddItemModel model)
+        {
+            var shoppingItem = await _context.ShoppingItems.FindAsync(model.ShoppingItemId);
+            if (shoppingItem is null)
+            {
+                return NotFound();
+            }
+
+            var scheduledShoppingItem = new ScheduledShoppingItem()
+            {
+                ShoppingItemId = shoppingItem.Id,
+                Day = model.Day
+            };
+
+            var entry = await _context.ScheduledShoppingItems.AddAsync(scheduledShoppingItem);
+            
+            await _context.SaveChangesAsync();
+
+            return Ok(entry.Entity);
+        }
+
+        [HttpPost("removeItem/{scheduledShoppingItemId}")]
+        public async Task<IActionResult> RemoveItem(int scheduledShoppingItemId)
+        {
+            var scheduledShoppingItem = await _context.ScheduledShoppingItems.FindAsync(scheduledShoppingItemId);
+            if (scheduledShoppingItem is null)
+            {
+                return NotFound();
+            }
+
+            _context.ScheduledShoppingItems.Remove(scheduledShoppingItem);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost("toggleBought/{scheduledShoppingItemId}")]
+        public async Task<IActionResult> ToggleBought(int scheduledShoppingItemId)
+        {
+            var scheduledShoppingItem = await _context.ScheduledShoppingItems.FindAsync(scheduledShoppingItemId);
+            if (scheduledShoppingItem is null)
+            {
+                return NotFound();
+            }
+
+            _context.Attach(scheduledShoppingItem);
+
+            scheduledShoppingItem.Bought = !scheduledShoppingItem.Bought;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(scheduledShoppingItem);
         }
 
         public IActionResult Privacy()
